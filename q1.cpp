025f6de38bd96adb4d1ede612ff04c7c40350800
Roman20000 <42239356+Roman20000@users.noreply.github.com>
2018-08-17 10:51:33 +0530

    Add files via upload

---
+++ q1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+int a; 
+float b;
+bool t;
+double e;
+cout <<"size of a is "<<sizeof(a)<<"\n";
+cout <<"size of b is "<<sizeof(b)<<"\n";
+cout <<"size of t is "<<sizeof(t)<<"\n";
+cout <<"size of e is "<<sizeof(e)<<"\n";
+return 0;
+}
+
+ 
