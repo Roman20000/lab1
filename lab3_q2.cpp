20fad19d2547c6bdffd75a68fce871315cf2fb0b
Roman20000 <42239356+Roman20000@users.noreply.github.com>
2018-08-17 11:23:26 +0530

    Add files via upload

---
+++ lab3_q2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+int a,b,c,d;
+float e;
+cout<<"a=7+5"<<"\n";
+cout<<"b=7+(-5)"<<"\n";
+cout<<"c=7*5"<<"\n";
+cout<<"d=5%7"<<"\n";
+cout<<"e=7/5"<<"\n";
+return 0;
+}
+
